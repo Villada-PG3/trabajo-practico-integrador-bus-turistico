d136882525231692f7b435e27e2093dfb18c4e4f
Valentino Olmedo <valentinomateoolmedo@gmail.com>
2025-08-11 14:19:20 +0000

    Urls dedicadas a usuario

--- proyecto_desarrollo/busturistico/urls_usuario.py
+++ proyecto_desarrollo/busturistico/urls_usuario.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views_usuario
 
 urlpatterns = [
-    path('', views.UsuarioInicioView.as_view(), name='usuario-inicio'),
-    path('recorridos/', views.UsuarioRecorridosView.as_view(), name='usuario-recorridos'),
-    path('recorridos/<int:pk>/', views.UsuarioDetalleRecorridoView.as_view(), name='usuario-detalle-recorrido'),
-    path('paradas/<int:pk>/', views.UsuarioDetalleParadaView.as_view(), name='usuario-detalle-parada'),
-    path('contacto/', views.UsuarioContactoView.as_view(), name='usuario-contacto'),
-
+    path('', views_usuario.UsuarioInicioView.as_view(), name='usuario-inicio'),
+    path('recorridos/', views_usuario.UsuarioRecorridosView.as_view(), name='usuario-recorridos'),
+    path('recorridos/<int:pk>/', views_usuario.UsuarioDetalleRecorridoView.as_view(), name='usuario-detalle-recorrido'),
+    path('paradas/<int:pk>/', views_usuario.UsuarioDetalleParadaView.as_view(), name='usuario-detalle-parada'),
+    path('contacto/', views_usuario.UsuarioContactoView.as_view(), name='usuario-contacto'),
+    path('busqueda/', views_usuario.UsuarioBusquedaView.as_view(), name='usuario-busqueda'),
 ]
