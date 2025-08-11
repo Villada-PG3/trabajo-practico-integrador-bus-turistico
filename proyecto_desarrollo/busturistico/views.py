331192cf81a0d449d0f263a3706d31d62edc1ea5
MatiasCarrera <matiasd1708@gmail.com>
2025-08-11 11:23:55 +0000

    todo

--- proyecto_desarrollo/busturistico/views.py
+++ proyecto_desarrollo/busturistico/views.py
@@ -1,149 +1,151 @@
-from django.shortcuts import render, redirect
+from django.views.generic import TemplateView, ListView, CreateView
+from django.shortcuts import redirect
 from django.utils import timezone
 from .models import Bus, Chofer, Viaje, EstadoBusHistorial, EstadoBus, EstadoViaje, Parada, Recorrido
-from .forms import ChoferForm, BusForm, ViajeForm, ParadaForm, RecorridoForm
-from django.db.models import Count, OuterRef, Subquery, F
-from django.utils import timezone
+from .forms import ChoferForm, BusForm
+from django.db.models import Count, OuterRef, Subquery
+from django.urls import reverse_lazy
 
 # Vistas principales del dashboard
-def dashboard_view(request):
-    estado_buses = []
-    for bus in Bus.objects.all():
-        historial = EstadoBusHistorial.objects.filter(patente_bus=bus).order_by('-fecha_inicio_estado').first()
-        estado = historial.estado_bus.nombre_estado if historial else 'Sin estado'
-        estado_buses.append((bus, estado))
-    choferes_activos = Chofer.objects.filter(activo=True)
-    ahora = timezone.now()
-    viajes_en_curso = Viaje.objects.filter(
-        fecha_hora_inicio_real__lte=ahora,
-        fecha_hora_fin_real__isnull=True
-    )
-    context = {'estado_buses': estado_buses, 'choferes_activos': choferes_activos, 'viajes_en_curso': viajes_en_curso}
-    return render(request, 'admin/dashboard.html', context)
+class DashboardView(TemplateView):
+    template_name = 'admin/dashboard.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        estado_buses = []
+        for bus in Bus.objects.all():
+            historial = EstadoBusHistorial.objects.filter(patente_bus=bus).order_by('-fecha_inicio_estado').first()
+            estado = historial.estado_bus.nombre_estado if historial else 'Sin estado'
+            estado_buses.append((bus, estado))
+        choferes_activos = Chofer.objects.filter(activo=True)
+        ahora = timezone.now()
+        viajes_en_curso = Viaje.objects.filter(
+            fecha_hora_inicio_real__lte=ahora,
+            fecha_hora_fin_real__isnull=True
+        )
+        context.update({
+            'estado_buses': estado_buses,
+            'choferes_activos': choferes_activos,
+            'viajes_en_curso': viajes_en_curso
+        })
+        return context
 
 # Vistas de gestión de entidades
-def choferes_view(request):
-    current_bus_subquery = Viaje.objects.filter(
-        chofer=OuterRef('pk'),
-        fecha_hora_inicio_real__isnull=False,
-        fecha_hora_fin_real__isnull=True
-    ).order_by('-fecha_hora_inicio_real').values('patente_bus__patente_bus')[:1]
+class ChoferesView(ListView):
+    template_name = 'admin/chofer.html'
+    model = Chofer
+    context_object_name = 'choferes_total'
 
-    choferes_total = Chofer.objects.annotate(
-        viajes_realizados=Count('viaje'),
-        bus_asignado_actual=Subquery(current_bus_subquery)
-    )
-    choferes_activos = choferes_total.filter(activo=True)
-    choferes_inactivos = choferes_total.filter(activo=False)
-    
-    context = {
-        'choferes_total': choferes_total,
-        'choferes_activos': choferes_activos,
-        'choferes_inactivos': choferes_inactivos,
-    }
-    return render(request, 'admin/chofer.html', context)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        current_bus_subquery = Viaje.objects.filter(
+            chofer=OuterRef('pk'),
+            fecha_hora_inicio_real__isnull=False,
+            fecha_hora_fin_real__isnull=True
+        ).order_by('-fecha_hora_inicio_real').values('patente_bus__patente_bus')[:1]
 
-def flota_view(request):
-    buses_total = Bus.objects.all()
-    buses_operativos = []
-    buses_reparacion = []
-    buses_mantenimiento = []
-    for bus in buses_total:
-        ultimo_estado_historial = EstadoBusHistorial.objects.filter(patente_bus=bus).order_by('-fecha_inicio_estado').first()
-        if ultimo_estado_historial:
-            estado = ultimo_estado_historial.estado_bus.nombre_estado.lower()
-            if estado == 'operativo':
-                buses_operativos.append(bus)
-            elif estado == 'en reparación':
-                buses_reparacion.append(bus)
-            elif estado == 'en mantenimiento':
-                buses_mantenimiento.append(bus)
-    context = {
-        'buses_total': buses_total,
-        'buses_operativos': buses_operativos,
-        'buses_reparacion': buses_reparacion,
-        'buses_mantenimiento': buses_mantenimiento,
-    }
-    return render(request, 'admin/flota.html', context)
+        choferes_total = Chofer.objects.annotate(
+            viajes_realizados=Count('viaje'),
+            bus_asignado_actual=Subquery(current_bus_subquery)
+        )
+        context.update({
+            'choferes_activos': choferes_total.filter(activo=True),
+            'choferes_inactivos': choferes_total.filter(activo=False),
+        })
+        return context
 
-def viajes_view(request):
-    viajes_en_curso = Viaje.objects.filter(estado_viaje__nombre_estado__iexact='en curso')
-    viajes_programados = Viaje.objects.filter(estado_viaje__nombre_estado__iexact='programado')
-    viajes_completados = Viaje.objects.filter(estado_viaje__nombre_estado__iexact='completado')
-    context = {
-        'viajes_en_curso': viajes_en_curso,
-        'viajes_programados': viajes_programados,
-        'viajes_completados': viajes_completados,
-    }
-    return render(request, 'admin/viajes.html', context)
+class FlotaView(TemplateView):
+    template_name = 'admin/flota.html'
 
-def paradas_view(request):
-    paradas = Parada.objects.all()
-    context = {'paradas': paradas}
-    return render(request, 'admin/paradas.html', context)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        buses_total = Bus.objects.all()
+        buses_operativos = []
+        buses_reparacion = []
+        buses_mantenimiento = []
+        for bus in buses_total:
+            ultimo_estado_historial = EstadoBusHistorial.objects.filter(patente_bus=bus).order_by('-fecha_inicio_estado').first()
+            if ultimo_estado_historial:
+                estado = ultimo_estado_historial.estado_bus.nombre_estado.lower()
+                if estado == 'operativo':
+                    buses_operativos.append(bus)
+                elif estado == 'en reparación':
+                    buses_reparacion.append(bus)
+                elif estado == 'en mantenimiento':
+                    buses_mantenimiento.append(bus)
+        context.update({
+            'buses_total': buses_total,
+            'buses_operativos': buses_operativos,
+            'buses_reparacion': buses_reparacion,
+            'buses_mantenimiento': buses_mantenimiento,
+        })
+        return context
 
-def recorridos_view(request):
-    recorridos = Recorrido.objects.all()
-    context = {'recorridos': recorridos}
-    return render(request, 'admin/recorridos.html', context)
+class ViajesView(TemplateView):
+    template_name = 'admin/viajes.html'
 
-def reportes_view(request):
-    return render(request, 'admin/reportes.html')
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'viajes_en_curso': Viaje.objects.filter(estado_viaje__nombre_estado__iexact='en curso'),
+            'viajes_programados': Viaje.objects.filter(estado_viaje__nombre_estado__iexact='programado'),
+            'viajes_completados': Viaje.objects.filter(estado_viaje__nombre_estado__iexact='completado'),
+        })
+        return context
+
+class ParadasView(ListView):
+    template_name = 'admin/paradas.html'
+    model = Parada
+    context_object_name = 'paradas'
+
+class RecorridosView(ListView):
+    template_name = 'admin/recorridos.html'
+    model = Recorrido
+    context_object_name = 'recorridos'
+
+class ReportesView(TemplateView):
+    template_name = 'admin/reportes.html'
 
 # Vistas para la creación de nuevos elementos
-def crear_chofer(request):
-    if request.method == 'POST':
-        form = ChoferForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('admin:admin-choferes')
-    else:
-        form = ChoferForm()
-    return render(request, 'admin/crear_chofer.html', {'form': form})
+class CrearChoferView(CreateView):
+    model = Chofer
+    form_class = ChoferForm
+    template_name = 'admin/chofer_form.html'
+    success_url = reverse_lazy('admin/chofer.html')
 
-def crear_bus(request):
-    if request.method == 'POST':
-        form = BusForm(request.POST)
-        if form.is_valid():
-            bus = form.save()
-            # Asignar estado inicial (por ejemplo, 'Operativo')
-            estado_inicial = EstadoBus.objects.get_or_create(nombre_estado='Operativo')[0]
-            EstadoBusHistorial.objects.create(
-                patente_bus=bus,
-                estado_bus=estado_inicial,
-                fecha_inicio_estado=timezone.now()
-            )
-            return redirect('admin:admin-flota')
-    else:
-        form = BusForm()
-    return render(request, 'admin/crear_bus.html', {'form': form})
+class CrearBusView(CreateView):
+    model = Bus
+    form_class = BusForm
+    template_name = 'admin/crear_bus.html'
+    success_url = reverse_lazy('admin:admin-flota')
 
-def crear_viaje(request):
-    if request.method == 'POST':
-        form = ViajeForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('admin:admin-viajes')
-    else:
-        form = ViajeForm()
-    return render(request, 'admin/crear_viaje.html', {'form': form})
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        estado_inicial = EstadoBus.objects.get_or_create(nombre_estado='Operativo')[0]
+        EstadoBusHistorial.objects.create(
+            patente_bus=self.object,
+            estado_bus=estado_inicial,
+            fecha_inicio_estado=timezone.now()
+        )
+        return response
 
-def crear_parada(request):
-    if request.method == 'POST':
-        form = ParadaForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('admin:admin-paradas')
-    else:
-        form = ParadaForm()
-    return render(request, 'admin/crear_parada.html', {'form': form})
+class CrearViajeView(TemplateView):
+    template_name = 'admin/crear_viaje.html'
 
-def crear_recorrido(request):
-    if request.method == 'POST':
-        form = RecorridoForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('admin:admin-recorridos')
-    else:
-        form = RecorridoForm()
-    return render(request, 'admin/crear_recorrido.html', {'form': form})
+    def post(self, request, *args, **kwargs):
+        # No form processing, just redirect to viajes page
+        return redirect('admin:admin-viajes')
+
+class CrearParadaView(TemplateView):
+    template_name = 'admin/crear_parada.html'
+
+    def post(self, request, *args, **kwargs):
+        # No form processing, just redirect to paradas page
+        return redirect('admin:admin-paradas')
+
+class CrearRecorridoView(TemplateView):
+    template_name = 'admin/crear_recorrido.html'
+
+    def post(self, request, *args, **kwargs):
+        # No form processing, just redirect to recorridos page
+        return redirect('admin:admin-recorridos')
