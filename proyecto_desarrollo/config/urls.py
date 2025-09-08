31a17faeeee161dc5c9ccd92ad8c21565b1b5a30
Valentino Olmedo <valentinomateoolmedo@gmail.com>
2025-09-08 13:43:34 +0000

    Solucion a problema al que se vea la foto del recorrido en vista usuario

--- proyecto_desarrollo/config/urls.py
+++ proyecto_desarrollo/config/urls.py
@@ -61,4 +61,8 @@ urlpatterns = [
     
     # Admin de Django
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+
+# Servir archivos media en desarrollo
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
