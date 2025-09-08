183e28956127e82c9df71531f9d14130b180f9e6
MatiasCarrera <matiasd1708@gmail.com>
2025-09-08 11:32:17 +0000

    Resuelto conflicto en urls_chofer.py

--- proyecto_desarrollo/busturistico/urls_chofer.py
+++ proyecto_desarrollo/busturistico/urls_chofer.py
@@ -1,13 +1,8 @@
-
 from django.urls import path
-from . import views_chofer
-
-
-
-
+from . import views
 
 urlpatterns = [
     path('mis-viajes/', views.mis_viajes, name="mis_viajes"),
     path('recorridos/', views.recorridos, name="recorridos"),
 ]
-
+    
