55ad2ffe019fd011cbdf86d08024ecc7a7847534
Valentino Olmedo <valentinomateoolmedo@gmail.com>
2025-09-15 13:10:46 +0000

    Agregado de boton en header para ver mapa

--- proyecto_desarrollo/busturistico/urls_usuario.py
+++ proyecto_desarrollo/busturistico/urls_usuario.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views_usuario
+from .views_usuario import MapaView
 
 urlpatterns = [
     path('', views_usuario.UsuarioInicioView.as_view(), name='usuario-inicio'),
@@ -7,5 +8,6 @@ urlpatterns = [
     path('recorridos/<int:pk>/', views_usuario.UsuarioDetalleRecorridoView.as_view(), name='usuario-detalle-recorrido'),
     path('paradas/<int:pk>/', views_usuario.UsuarioDetalleParadaView.as_view(), name='usuario-detalle-parada'),
     path('contacto/', views_usuario.UsuarioContactoView.as_view(), name='usuario-contacto'),
+    path('mapa/', MapaView.as_view(), name='usuario-mapa'),
     path('busqueda/', views_usuario.UsuarioBusquedaView.as_view(), name='usuario-busqueda'),
 ]
