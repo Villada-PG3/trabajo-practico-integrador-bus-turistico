e9cd1a46041b02a3a7453ce2010121ee22d65b1c
Valentino Olmedo <valentinomateoolmedo@gmail.com>
2025-09-15 14:47:18 +0000

    Html limpio: Saqué el css de dentro de los archivos html

--- proyecto_desarrollo/busturistico/urls_usuario.py
+++ proyecto_desarrollo/busturistico/urls_usuario.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views_usuario
 from .views_usuario import MapaView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views_usuario.UsuarioInicioView.as_view(), name='usuario-inicio'),
@@ -11,3 +13,7 @@ urlpatterns = [
     path('mapa/', MapaView.as_view(), name='usuario-mapa'),
     path('busqueda/', views_usuario.UsuarioBusquedaView.as_view(), name='usuario-busqueda'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
