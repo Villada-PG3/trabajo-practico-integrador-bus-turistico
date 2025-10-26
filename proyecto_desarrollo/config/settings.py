e490d6871a4244b0b848f79d151f0c6fbf25ef35
MatiasCarrera <matiasd1708@gmail.com>
2025-10-26 23:33:36 +0000

    Settings final

--- proyecto_desarrollo/config/settings.py
+++ proyecto_desarrollo/config/settings.py
@@ -23,6 +23,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'material',
     'material.admin',
+    
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
