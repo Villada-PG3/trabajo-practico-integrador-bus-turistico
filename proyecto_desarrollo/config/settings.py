e9e3195c51f34ac0e4b0c63a443f75392028e785
MatiasCarrera <matiasd1708@gmail.com>
2025-10-26 23:28:21 +0000

    Settings final

--- proyecto_desarrollo/config/settings.py
+++ proyecto_desarrollo/config/settings.py
@@ -21,14 +21,14 @@ ALLOWED_HOSTS = []
 
 # Application definition
 INSTALLED_APPS = [
+    'material',
+    'material.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'busturistico',  # Tu app personalizada
-    'material',
-    'material.admin',
 ]
 
 MIDDLEWARE = [
